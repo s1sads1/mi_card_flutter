a8e4cc201e68fb875037e6c54e1d81e4ff4c301a
박선호 <s2sads2@naver.com>
2022-03-07 14:36:42 +0900

    [FEAT] MI_CARD

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 50.0,
+                backgroundImage: AssetImage('images/lion.jpeg'),
+              ),
+              Text(
+                  'Lion',
+                style: TextStyle(
+                  fontFamily: 'Oswald-Bold',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '010-3403-****',
+                        style: TextStyle(
+                          color: Colors.teal.shade900
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0
+                ),
+                  child: ListTile(
+                    leading:
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title:
+                      Text(
+                        's2sads*@naver.com',
+                        style: TextStyle(
+                            color: Colors.teal.shade900
+                        ),
+                      ),
+                  ),
+                ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
